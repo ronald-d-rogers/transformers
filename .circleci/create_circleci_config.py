57b7ceaab4ae5d9ee49743ec7b07f9441e5f557a
ydshieh <ydshieh@users.noreply.github.com>
2023-05-04 11:39:49 +0200

    run

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -440,15 +440,15 @@ doc_test_job = CircleCIJob(
 )
 
 REGULAR_TESTS = [
-    torch_and_tf_job,
-    torch_and_flax_job,
-    torch_job,
-    tf_job,
-    flax_job,
-    custom_tokenizers_job,
-    hub_job,
-    onnx_job,
-    exotic_models_job,
+    # torch_and_tf_job,
+    # torch_and_flax_job,
+    # torch_job,
+    # tf_job,
+    # flax_job,
+    # custom_tokenizers_job,
+    # hub_job,
+    # onnx_job,
+    # exotic_models_job,
     doc_test_job
 ]
 EXAMPLES_TESTS = [
@@ -474,8 +474,8 @@ def create_circleci_config(folder=None):
             all_test_list = f.read()
     else:
         all_test_list = []
-    if len(all_test_list) > 0:
-        jobs.extend(PIPELINE_TESTS)
+    # if len(all_test_list) > 0:
+    #     jobs.extend(PIPELINE_TESTS)
 
     test_file = os.path.join(folder, "filtered_test_list.txt")
     if os.path.exists(test_file):
@@ -515,12 +515,12 @@ def create_circleci_config(folder=None):
             fp.write(" ".join(extended_tests_to_run))
 
     example_file = os.path.join(folder, "examples_test_list.txt")
-    if os.path.exists(example_file) and os.path.getsize(example_file) > 0:
-        jobs.extend(EXAMPLES_TESTS)
+    # if os.path.exists(example_file) and os.path.getsize(example_file) > 0:
+    #     jobs.extend(EXAMPLES_TESTS)
 
     repo_util_file = os.path.join(folder, "test_repo_utils.txt")
-    if os.path.exists(repo_util_file) and os.path.getsize(repo_util_file) > 0:
-        jobs.extend(REPO_UTIL_TESTS)
+    # if os.path.exists(repo_util_file) and os.path.getsize(repo_util_file) > 0:
+    #     jobs.extend(REPO_UTIL_TESTS)
 
     if len(jobs) > 0:
         config = {"version": "2.1"}
